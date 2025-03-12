9a4a5d0b83d783f7eef44e7df41245224e21aba8
Nikita Skulkin <60268027+sklknn@users.noreply.github.com>
2025-03-12 13:18:24 +0300

    update task8

---
+++ task8/install_yccli.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        # any linux
+    echo 'Installing for Linux'
+    curl -sSL https://storage.yandexcloud.net/yandexcloud-yc/install.sh | bash
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        # Mac OSX
+    echo 'Installing for Mac OS'
+    curl -sSL https://storage.yandexcloud.net/yandexcloud-yc/install.sh | bash
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+        # POSIX compatibility layer and Linux environment emulation for Windows (it's WSL)
+elif [[ "$OSTYPE" == "msys" ]]; then
+        # mingw git-bash
+    echo 'Installing for Windows'
+    pwsh iex (New-Object System.Net.WebClient).DownloadString('https://storage.yandexcloud.net/yandexcloud-yc/install.ps1')
+fi
